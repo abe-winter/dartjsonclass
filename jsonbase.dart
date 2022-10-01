813d9e01c09dd9bc3b61e34f599520f66088d9fc
Abe Winter <awinter.public@gmail.com>
2022-09-30 20:20:41 -0400

    bundle jsonbase with pkg

--- jsonbase.dart
+++ jsonbase.dart
@@ -1,12 +1,1 @@
-import 'dart:convert';
-
-/// base class for json messages
-abstract class JsonBase {
-  Map<String, dynamic> toMap();
-
-  String toJson() => jsonEncode(toMap());
-
-  // dart why won't you love me
-  // static JsonBase fromMap(Map<String, dynamic> map) => throw UnimplementedError("I'm abstract even though that's not allowed");
-  // static JsonBase fromJson(String raw) => fromMap(jsonDecode(raw) as Map<String, dynamic>);
-}
+dartjsonclass/jsonbase.dart
